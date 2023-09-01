78318e2a3d7a4b1f88f4cf839a081e6cd819c665
JAPNEET30 <japneetsinghjdp@gmail.com>
2023-09-02 00:23:44 +0530

    added files

--- Project1/cpu6502.cpp
+++ Project1/cpu6502.cpp
@@ -1,23 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+using Byte = unsigned char;
+using Word = unsigned short;
+using u32 = unsigned int;
+struct Mem
+{
+	static constexpr u32 MAX_MEM = 1024 * 64;
+	Byte Data[MAX_MEM];
+	void Initialize()
+	{
+		for (u32 i = 0; i < MAX_MEM; i++) {
+			Data[i] = 0;
+		}
+	}
+
+	//read 1 byte
+	Byte operator[](u32 Address) const
+	{
+		//assert here Address is < MAX_MEM
+		return Data[Address];
+	}
+
+	//write 1 Byte
+	Byte& operator[](u32 Address)
+	{
+		//assert here Address is < MAX_MEM
+		return Data[Address];
+	}
+};
 struct CPU {
-	using Byte = unsigned char;
-	using Word = unsigned short;
+	
 
-	Word PC;
-	Word SP;
+	Word PC;  //Program counter
+	Word SP;  //stack pointer
 
-	Byte A, X, Y;
+	Byte A, X, Y;	//registers
 
-	Byte C : 1;
-	Byte Z : 1;
-	Byte I : 1;
-	Byte D : 1;
-	Byte B : 1;
-	Byte V : 1;
-	Byte N : 1;
+	Byte C : 1;	//status flag
+	Byte Z : 1;	//status flag
+	Byte I : 1;	//status flag
+	Byte D : 1;	//status flag
+	Byte B : 1;	//status flag
+	Byte V : 1;	//status flag
+	Byte N : 1;	//status flag
+
+	void Reset(Mem& memory)
+	{
+		PC = 0xFFFC;
+		SP = 0x0100;
+		C = Z = I = D = B = V = N = 0;
+		A = X = Y = 0;
+		memory.Initialize();
+	}
+	Byte FetchByte(u32 Cycles, Mem& memory)
+	{
+		Byte DATA = memory[PC];
+		PC++;
+		Cycles--;
+		return DATA;
+	}
+
+	//opcode
+	static constexpr Byte
+		INS_LDA_IM = 0xA9;
+
+	void Execute(u32 Cycles, Mem& memory)
+	{
+		while(Cycles > 0)
+		{
+			Byte Ins = FetchByte(Cycles, memory);
+			switch (Ins)
+			{
+			case INS_LDA_IM:
+			{
+				Byte Value = FetchByte(Cycles, memory);
+				A = Value;
+				Z = (A == 0);
+				N = (A & 0b10000000) > 0;
+
+			}break;
+			default:
+			{
+				printf("Instruction not handled %d", Ins); 
+			}
+			}
+		}
+	}
 };
 int main() {
+	Mem mem;
+	CPU cpu;
+	cpu.Reset(mem);
+	//start - inline a little program
+	mem[0xFFFC] = CPU::INS_LDA_IM;
+	mem[0xFFFD] = 0x42;
+	//end - inline little program
+	cpu.Execute(2, mem);
 	return 0;
 }
